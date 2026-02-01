036a5bb81414d125a53894d54a1c140b20e36048
PrateekRajput12 <ficredinn@gmail.com>
2026-02-01 15:57:16 +0530

    done

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -123,8 +123,8 @@ export const ProjectsSection = () => {
 
               <div className="p-6">
                 <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                  {project.tags.map((tag, i) => (
+                    <span key={i} className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
                       {tag}
                     </span>
                   ))}
